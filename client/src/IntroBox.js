1c9f9e4ac57c806997d3ac75e4fa13c688a74046
yw-zhou <yiweizhou123@hotmail.com>
2021-01-24 16:42:41 -0500

    add reset pass UI

--- client/src/IntroBox.js
+++ client/src/IntroBox.js
@@ -6,7 +6,7 @@ class IntroBox extends Component {
         mode: 'title'
     }
     submit(e) {
-        if (this.state.mode === 'SIGN UP') {
+        if (this.state.mode === 'Sign Up') {
             if (this.state.password !== this.state.verify) {
                 return
             }
@@ -21,8 +21,13 @@ class IntroBox extends Component {
         this.setState({[e.target.id]: e.target.value})
     }
     toggle() {
-        let mode = (this.state.mode === 'SIGN UP') ? 'LOG IN' : 'SIGN UP';
+        let mode = (this.state.mode === 'Sign Up') ? 'Log In' : 'Sign Up';
         this.setState({mode: mode})
+    }
+    submit_messages = {
+        'Sign Up': 'Create Account',
+        'Log In': 'Sign In',
+        'Password Reset': 'Send Email'
     }
     render() {
         let main = (
@@ -30,8 +35,8 @@ class IntroBox extends Component {
                 <h1 id="main-title">Let's Study Together!</h1>
                 <p>During Covid blah blah blah (some explanation of helping each other)</p>
                 <div>
-                    <button type="button" className="btn btn-dark theme-light mx-2" onClick={() => this.setState({ mode: 'SIGN UP' })} >Sign Up</button>
-                    <button type="button" className="btn btn-light mx-2" onClick={() => this.setState({ mode: 'LOG IN' })}>Log In</button>
+                    <button type="button" className="btn btn-dark theme-light mx-2" onClick={() => this.setState({ mode: 'Sign Up' })} >Sign Up</button>
+                    <button type="button" className="btn btn-light mx-2" onClick={() => this.setState({ mode: 'Log In' })}>Log In</button>
                 </div>
             </div>
         )
@@ -44,14 +49,19 @@ class IntroBox extends Component {
                         <div className="input-group mb-1">
                             <input onChange={this.handlechange.bind(this)} type="text" className="form-control" id="email" aria-label="Email" aria-describedby="basic-addon2" />
                         </div>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <label className="input-label">Password</label>
-                            {this.state.mode === 'LOG IN' ? <button type="button" className="btn btn-link">Forgot Password?</button> : null}
-                        </div>
-                        <div className="input-group mb-1">
-                            <input onChange={this.handlechange.bind(this)} type="password" className="form-control" id="password" />
-                        </div>
-                        {this.state.mode === 'SIGN UP' ?
+                        {this.state.mode !== 'Password Reset' ?
+                            (<div>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <label className="input-label">Password</label>
+                                    {this.state.mode === 'Log In' ? <button type="button" className="btn btn-link" onClick={()=>{this.setState({mode:'Password Reset'})}}>Forgot Password?</button> : null}
+                                </div>
+                                <div className="input-group mb-1">
+                                    <input onChange={this.handlechange.bind(this)} type="password" className="form-control" id="password" />
+                                </div>
+                            </div>) :
+                            (<p class="font-weight-light text-left text-muted" style={{fontSize:14}}>You will receive an email with a password reset link.</p>)
+                        }
+                        {this.state.mode === 'Sign Up' ?
                             (<div>
                                 <label className="input-label m-0">Verify Password</label>
                                 <div className="input-group mb-1">
@@ -59,15 +69,16 @@ class IntroBox extends Component {
                                 </div>
                             </div>) : null
                         }
-                        <button type="submit" className="btn btn-dark theme-light w-100 mx-0 my-3" onClick={this.submit.bind(this)}>{this.state.mode === 'LOG IN' ? 'Sign In' : 'Create Account'}</button>
+                        <button type="submit" className="btn btn-dark theme-light w-100 mx-0 my-3" onClick={this.submit.bind(this)}>{this.submit_messages[this.state.mode]}</button>
                         
-                        <div className="card">
-                            <div className="card-body input-label p-1 d-flex justify-content-center align-items-center">
-                                <p className="m-0">{ this.state.mode === 'LOG IN' ? 'New to Study With Me?' : 'Already have an account?' }</p>
-                                <button type="button" className="btn btn-link" onClick={this.toggle.bind(this)}>{this.state.mode === 'LOG IN' ? 'Create an Account' : 'Log in Here'}</button>
-                            </div>
-                        </div>
-                        
+                        {this.state.mode !== 'Password Reset' ?
+                            (<div className="card">
+                                <div className="card-body input-label p-1 d-flex justify-content-center align-items-center">
+                                    <p className="m-0">{this.state.mode === 'Log In' ? 'New to Study With Me?' : 'Already have an account?'}</p>
+                                    <button type="button" className="btn btn-link" onClick={this.toggle.bind(this)}>{this.state.mode === 'Log In' ? 'Create an Account' : 'Log in Here'}</button>
+                                </div>
+                            </div>) : null
+                        }
                     </div>
                 </div>
             )
