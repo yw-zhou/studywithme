dd1b7bd6ef24b6bef9eeb7be22479552f68d30ac
yw-zhou <yiweizhou123@hotmail.com>
2021-01-24 16:20:26 -0500

    UI fixes

--- client/src/IntroBox.js
+++ client/src/IntroBox.js
@@ -20,6 +20,10 @@ class IntroBox extends Component {
     handlechange(e) {
         this.setState({[e.target.id]: e.target.value})
     }
+    toggle() {
+        let mode = (this.state.mode === 'SIGN UP') ? 'LOG IN' : 'SIGN UP';
+        this.setState({mode: mode})
+    }
     render() {
         let main = (
             <div>
@@ -51,19 +55,19 @@ class IntroBox extends Component {
                             (<div>
                                 <label className="input-label m-0">Verify Password</label>
                                 <div className="input-group mb-1">
-                                    <input onChange={this.handlechange.bind(this)} type="verify" className="form-control" id="verify" />
+                                    <input onChange={this.handlechange.bind(this)} type="password" className="form-control" id="verify" />
                                 </div>
                             </div>) : null
                         }
                         <button type="submit" className="btn btn-dark theme-light w-100 mx-0 my-3" onClick={this.submit.bind(this)}>{this.state.mode === 'LOG IN' ? 'Sign In' : 'Create Account'}</button>
-                        {this.state.mode === 'LOG IN' ?
-                            (<div className="card">
-                                <div className="card-body input-label p-1 d-flex justify-content-center align-items-center">
-                                    <p className="m-0">New to Study With Me?</p>
-                                    <button type="button" className="btn btn-link" onClick={() => this.setState({ mode: 'SIGN UP' })}>Create an Account</button>
-                                </div>
-                            </div>) : null
-                        }
+                        
+                        <div className="card">
+                            <div className="card-body input-label p-1 d-flex justify-content-center align-items-center">
+                                <p className="m-0">{ this.state.mode === 'LOG IN' ? 'New to Study With Me?' : 'Already have an account?' }</p>
+                                <button type="button" className="btn btn-link" onClick={this.toggle.bind(this)}>{this.state.mode === 'LOG IN' ? 'Create an Account' : 'Log in Here'}</button>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             )
