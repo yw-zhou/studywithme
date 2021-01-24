c30bf832eeebd6e34a62b0ade3d76231490cc96a
yw-zhou <yiweizhou123@hotmail.com>
2021-01-23 19:13:27 -0500

    titlepage setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const size = useWindowSize()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles
+        params={{
+          "particles": {
+              "number": {
+                  "value": size.width*size.height/10000
+              },
+              "size": {
+                  "value": 3
+              }
+          },
+          "interactivity": {
+              "events": {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+              }
+          },
+      }} />
+      <div class="title-page-centre">
+        <h1 id="main-title">Let's Study Together!</h1>
+        <p>During Covid blah blah blah (some explanation of helping each other)</p>
+        <div>
+          <button type="button" class="btn btn-dark theme-light">Sign Up</button>
+          <button type="button" class="btn btn-light">Log In</button>
+        </div>
+      </div>
     </div>
   );
+}
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); 
+  return windowSize;
 }
 
 export default App;
