f7a70eb7d0cf10aae4c348817f08362e5fb3847c
yw-zhou <yiweizhou123@hotmail.com>
2021-03-18 22:43:25 -0400

    edit account ui

--- client/src/Main/UserInfo.js
+++ client/src/Main/UserInfo.js
@@ -42,7 +42,13 @@ class UserInfo extends Component {
   }
 
   handleChange(e) {
-    this.setState({ searchCode: e.target.value });
+    this.setState({ [e.target.id]: e.target.value });
+  }
+
+  updateAccountInfo() {
+    // axios.post(
+    //   `http://localhost:9000/group/getGroups?user=${this.props.email}`
+    // );
   }
 
   render() {
@@ -57,77 +63,125 @@ class UserInfo extends Component {
           src={ProfilePic}
           alt="profile pic"
         />
-        <div className="userinfo">
-          <h5>{this.state.userData.name}</h5>
-          <p>{this.state.userData.intro}</p>
-        </div>
-        <div className="groups">
-          <div className="d-flex align-items-center">
-            <h3 className="text-secondary">Groups</h3>
-            <span
-              className="d-inline-block ms-auto"
-              data-bs-toggle="tooltip"
-              title="Add Group"
-              data-bs-animation="true"
-            >
-              <button
-                type="button"
-                className="btn pt-0 pb-1 px-1"
-                onClick={this.props.createNewGroup}
-              >
-                <i className="bi bi-plus-circle text-secondary"></i>
-              </button>
-            </span>
-            <span
-              className="d-inline-block"
-              data-bs-toggle="tooltip"
-              title="Explore Public Groups"
-              data-bs-animation="true"
-            >
-              <button type="button" className="btn pt-0 pb-1 px-1">
-                <i className="bi bi-compass text-secondary"></i>
-              </button>
-            </span>
-          </div>
-          <div className="input-group mb-3">
+        {this.state.editUser && (
+          <div>
+            <label className="input-label text-secondary">Name/Alias</label>
             <input
               type="text"
               className="form-control border-secondary text-info bg-light"
-              placeholder="Group Code"
-              aria-label="Group Code"
+              aria-label="Meeting Name"
               aria-describedby="button-addon2"
+              value={this.state.userData.name}
+              id="userName"
               onChange={this.handleChange.bind(this)}
             />
+            <textarea
+              className="form-control border-secondary text-info bg-light mt-3"
+              placeholder="Introduction (Optionional)"
+              id="intro"
+              onChange={this.handleChange.bind(this)}
+            ></textarea>
+
             <button
-              className="btn btn-outline-secondary"
               type="button"
-              id="button-addon2"
-              onClick={() => this.props.handleSearchCode(this.state.searchCode)}
+              className="float-left px-0 font-size-14 btn btn-link text-secondary"
             >
-              <i className="bi bi-search"></i>
+              Reset Password
+            </button>
+            <button
+              onClick={this.updateAccountInfo}
+              type="submit"
+              className="btn btn-outline-secondary w-25 float-right mt-2"
+            >
+              Save
             </button>
           </div>
-          <div className="rounded d-flex flex-column">
-            {this.state.groups.map((group) => (
-              <button
-                type="button"
-                className={`noFocus btn text-info ${
-                  this.state.selectedGroup === group.groupId
-                    ? "btn-secondary"
-                    : ""
-                }`}
-                onClick={this.handleGroupSelect.bind(this)}
-                id={group.groupId}
-                key={group.groupId}
-              >
-                <li id={group.groupId}>{group.groupName}</li>
-              </button>
-            ))}
-            {this.state.groups.length ? null : (
-              <p className="m-3">'No Groups at the moment, Join above!'</p>
-            )}
+        )}
+
+        {!this.state.editUser && (
+          <div>
+            <div className="userinfo">
+              <div className="d-flex align-items-center">
+                <i
+                  onClick={() => this.setState({ editUser: true })}
+                  className="bi bi-gear-fill mx-1"
+                ></i>
+                <h5 className="m-0 username">{this.state.userData.name}</h5>
+              </div>
+              <p>{this.state.userData.intro}</p>
+            </div>
+            <div className="groups">
+              <div className="d-flex align-items-center">
+                <h3 className="text-secondary">Groups</h3>
+                <span
+                  className="d-inline-block ms-auto"
+                  data-bs-toggle="tooltip"
+                  title="Add Group"
+                  data-bs-animation="true"
+                >
+                  <button
+                    type="button"
+                    className="btn pt-0 pb-1 px-1"
+                    onClick={this.props.createNewGroup}
+                  >
+                    <i className="bi bi-plus-circle text-secondary"></i>
+                  </button>
+                </span>
+                <span
+                  className="d-inline-block"
+                  data-bs-toggle="tooltip"
+                  title="Explore Public Groups"
+                  data-bs-animation="true"
+                >
+                  <button type="button" className="btn pt-0 pb-1 px-1">
+                    <i className="bi bi-compass text-secondary"></i>
+                  </button>
+                </span>
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control border-secondary text-info bg-light"
+                  placeholder="Group Code"
+                  aria-label="Group Code"
+                  aria-describedby="button-addon2"
+                  id="SearchCode"
+                  onChange={this.handleChange.bind(this)}
+                />
+                <button
+                  className="btn btn-outline-secondary"
+                  type="button"
+                  id="button-addon2"
+                  onClick={() =>
+                    this.props.handleSearchCode(this.state.searchCode)
+                  }
+                >
+                  <i className="bi bi-search"></i>
+                </button>
+              </div>
+              <div className="rounded d-flex flex-column">
+                {this.state.groups.map((group) => (
+                  <button
+                    type="button"
+                    className={`noFocus btn text-info ${
+                      this.state.selectedGroup === group.groupId
+                        ? "btn-secondary"
+                        : ""
+                    }`}
+                    onClick={this.handleGroupSelect.bind(this)}
+                    id={group.groupId}
+                    key={group.groupId}
+                  >
+                    <li id={group.groupId}>{group.groupName}</li>
+                  </button>
+                ))}
+                {this.state.groups.length ? null : (
+                  <p className="m-3">'No Groups at the moment, Join above!'</p>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
